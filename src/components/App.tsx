6bb3368a9fa90180ff9956970858c95b9e912bb4
Ваше Имя <you@example.com>
2025-12-10 15:48:56 +0500

    refactor

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,45 +1,21 @@
-import { ReactElement, useEffect, useMemo, useState } from 'react';
-
-import { useSelector } from 'react-redux';
-
-import { FilterState, INITIAL_PAGE, TASK_PER_PAGE } from '@/models/constants';
-import { Task } from '@/store/todoSlice';
+import { ReactElement} from 'react';
 
 import { Filters } from './Filters';
 import { Form } from './Form';
 import { List } from './List';
 import { Pagination } from './Pagination';
+import { useTodo } from './useTodo';
 
 import classes from './App.module.scss';
 
 export const App = (): ReactElement => {
-  const [activeFilter, setFilter] = useState(FilterState.ALL);
-  const [currentPage, setCurrentPage] = useState(INITIAL_PAGE);
-  const list: Task[] = useSelector((state: {tasks: {tasks: Task[]}}) => state.tasks.tasks)
-
-  useEffect(() => {
-    setCurrentPage(INITIAL_PAGE);
-  }, [activeFilter]);
-
-  const getFilteredList = useMemo((): Task[] => {
-    if (activeFilter === FilterState.ALL) {
-      return list;
-    }
-    return list.filter((task) =>
-      activeFilter === FilterState.COMPLETE ? task.isComplete : !task.isComplete,
-    );
-  }, [list, activeFilter]);
-
-  const start: number = (currentPage - INITIAL_PAGE) * TASK_PER_PAGE;
-  const end: number = currentPage * TASK_PER_PAGE;
-  const totalPages: number = Math.ceil(getFilteredList.length / TASK_PER_PAGE);
-  const paginatedTask: Task[] = getFilteredList.slice(start, end);
+  const { tasks, totalPages, activeFilter, setFilter, currentPage, setCurrentPage } = useTodo();
 
   return (
     <>
       <h1 className={classes.header}>ToDo</h1>
-      <Form/>
-      <List list={paginatedTask}/>
+      <Form />
+      <List list={tasks} />
       <Filters onSetActiveFilter={setFilter} activeFilter={activeFilter} />
       <Pagination
         totalPages={totalPages}
---
+++ src/components/useTodo.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useMemo, useState } from 'react';
+
+import { useSelector } from 'react-redux';
+
+import { FilterState, INITIAL_PAGE, TASK_PER_PAGE } from '@/models/constants';
+import { Task } from '@/store/todoSlice';
+
+
+
+export const useTodo = () => {
+  const [activeFilter, setFilter] = useState(FilterState.ALL);
+  const [currentPage, setCurrentPage] = useState(INITIAL_PAGE);
+  const list: Task[] = useSelector((state: { tasks: { tasks: Task[] } }) => state.tasks.tasks);
+
+  useEffect(() => {
+    setCurrentPage(INITIAL_PAGE);
+  }, [activeFilter]);
+
+  const getFilteredList = useMemo((): Task[] => {
+    if (activeFilter === FilterState.ALL) {
+      return list;
+    }
+    return list.filter((task) =>
+      activeFilter === FilterState.COMPLETE ? task.isComplete : !task.isComplete,
+    );
+  }, [list, activeFilter]);
+
+  const start: number = (currentPage - INITIAL_PAGE) * TASK_PER_PAGE;
+  const end: number = currentPage * TASK_PER_PAGE;
+  const totalPages: number = Math.ceil(getFilteredList.length / TASK_PER_PAGE);
+  const paginatedTask: Task[] = getFilteredList.slice(start, end);
+
+  return {
+    tasks: paginatedTask,
+    totalPages,
+    activeFilter,
+    setFilter,
+    currentPage,
+    setCurrentPage,
+  };
+};
