2bdc2600dc6f4e3f60a7fd137888de9900c418ad
Ваше Имя <you@example.com>
2025-12-10 15:01:55 +0500

    refactor

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -6,7 +6,9 @@ import { deleteTask, toggleStatusTask } from '@/store/todoSlice';
 import classes from './index.module.scss';
 
 export const Item = ({ task: { text, id, isComplete } }: TodoItemProps) => {
+
   const dispatch = useDispatch();
+  
   return (
     <li>
       <button onClick={() => dispatch(deleteTask({ id }))}>Delete</button>
